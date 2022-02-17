ebed76c7c60a955ad4081274b765156014ed7977
Luis Fernando <lsh.fernandors@gmail.com>
2022-02-17 02:13:35 -0300

    add tokenurl

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -37,10 +37,13 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      accessTokenUrl: "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
       authorization: {
-        url: "https://ignews-gamma-blue.vercel.app/api/auth/callback/google",
-      }
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      },
     }),
   ],
   theme: {
